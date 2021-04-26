6ffd3a4d9f1c2baaae9d89d2e28a37a11860865e
liqiqiorz <1099462011@qq.com>
2021-04-26 11:22:29 +0800

    add:ReadMe:my plan

--- main/structTranslateMap.go
+++ main/structTranslateMap.go
@@ -30,3 +30,19 @@ func main() {
 //key:age value:18
 //看起来没什么问题，但其实这里是有一个“坑”的。那就是Go语言中的json包在序列化空接口存放的数字类型
 //（整型、浮点型等）都会序列化成float64类型。
+
+//第三方库structs
+//除了自己实现，Github上也有现成的轮子，例如第三方库：https://github.com/fatih/structs。
+//
+//它使用的自定义结构体tag是structs:
+
+// UserInfo 用户信息
+//type UserInfo struct {
+//	Name string `json:"name" structs:"name"`
+//	Age  int    `json:"age" structs:"age"`
+//}
+//用法很简单：
+//m3 := structs.Map(&u1)
+//for k, v := range m3 {
+//fmt.Printf("key:%v value:%v value type:%T\n", k, v, v)
+//}
