ce6adf4ff27b50913a7b789ad70a41242f44c61c
liqiqiorz <1099462011@qq.com>
2021-06-20 10:55:30 +0800

    feat:每日一题

---
+++ leetCode/T102.go
@@ -0,0 +1,24 @@
+package leetCode
+
+/*
+*  @author liqiqiorz
+*  @data 2021/6/20 10:45
+ */
+var res [][]int
+
+func levelOrder(root *TreeNode) [][]int {
+	res = [][]int{}
+	dfs(root, 0)
+	return res
+}
+
+func dfs(root *TreeNode, level int) {
+	if root != nil {
+		if level == len(res) {
+			res = append(res, []int{})
+		}
+		res[level] = append(res[level], root.Val)
+		dfs(root.Left, level+1)
+		dfs(root.Right, level+1)
+	}
+}
---
+++ leetCode/TreeNode.go
@@ -0,0 +1,11 @@
+package leetCode
+
+/*
+*  @author liqiqiorz
+*  @data 2021/6/20 10:52
+ */
+
+type TreeNode struct {
+	Right, Left *TreeNode
+	Val         int
+}
