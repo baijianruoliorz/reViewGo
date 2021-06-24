e3f54eb59fc8e5b94835326f009e063cc17cd424
liqiqiorz <1099462011@qq.com>
2021-06-24 10:44:05 +0800

    feat:T33

---
+++ leetCode/T33.go
@@ -0,0 +1,19 @@
+package leetCode
+
+/*
+*  @author liqiqiorz
+*  @data 2021/6/24 10:26
+ */
+
+func findMin(nums []int) int {
+	low, high := 0, len(nums)-1
+	for low < high {
+		pivot := low + (high-low)/2
+		if nums[pivot] < nums[high] {
+			high = pivot
+		} else {
+			low = pivot + 1
+		}
+	}
+	return nums[low]
+}
